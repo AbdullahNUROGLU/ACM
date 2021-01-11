b2f0a7fcd8a257a9d0a2bc7031737f62eaf73ba0
Abdallahkajj <busy.heart@live.com>
2021-01-11 19:26:45 +0300

    Address Class added

---
+++ ACM.BL/Address.cs
@@ -0,0 +1,30 @@
+﻿namespace ACM.BL
+{
+    internal class Address
+    {
+        public int AddressId { get; set; }
+        public int AddressType { get; set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
+        public Address()
+        {
+        }
+
+        public Address(int addressId)
+        {
+            AddressId = addressId;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+            if (PostalCode == null) isValid = false;
+            return isValid;
+        }
+    }
+}
