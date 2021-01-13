4407efc35cadced6223919d42f89aad5c87fd8bb
Abdallahkajj <busy.heart@live.com>
2021-01-13 18:37:14 +0300

    String Handler with it's test

---
+++ Tests/Acme.CommonTest/StringHandlerTest.cs
@@ -0,0 +1,38 @@
+﻿using Acme.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Acme.CommonTest
+{
+    [TestClass]
+    public class StringHandlerTest
+    {
+        [TestMethod]
+        public void IsertSpaceTestValid()
+        {
+            //Arrange
+            StringHandler stringHandler = new StringHandler();
+            string expexted = "Name Test";
+
+            //Act
+            string actual = stringHandler.InsertSpaces("NameTest");
+
+            //Assert
+            Assert.AreEqual(expexted, actual);
+        }
+
+        [TestMethod]
+        public void IsertSpaceTestWithExistingSpace()
+        {
+            //Arrange
+            StringHandler stringHandler = new StringHandler();
+            string expexted = "Name Test";
+
+            //Act
+            string actual = stringHandler.InsertSpaces("Name Test");
+
+            //Assert
+            Assert.AreEqual(expexted, actual);
+        }
+    }
+}
